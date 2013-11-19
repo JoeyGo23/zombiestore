984c2dfad80c3cc830ca87ee9f0ed80d55bfd4d6
Joe Gomez <joegomez@Joes-MacBook-Pro.local>
2013-11-18 20:11:21 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZombieStore::Application.config.secret_key_base = 'a06a0365d955d34903ada50ec918c931d77c7032885d69c8ae0ec4c8a093c001cc9cd48e91fe7ffaca60f6a40f41fcfcb19055098f1a5e38ec90d7c62abce389'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+ZombieStore::Application.routes.draw do
+  root 'products#index'
+  end
